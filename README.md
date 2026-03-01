3f3a35fb3d48a5c12d8160d2b2cb526c40882e2c
Aaditya Anand <aa3014429@gmail.com>
2026-03-02 00:13:34 +0530

    Added async to chatbot

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A basic and simple agent that uses rwkv models.
 
 ## progress
 - [x] Implement a chatbot with system prompts.
+- [x] Work it to run on async.
 - [ ] Add RAG system with to it.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import asyncio
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, chat
 
 llm_name: str = "rwkv7-g1d"
 provider_url: str = "http://127.0.0.1:65530/api/oai"
@@ -10,21 +11,36 @@ template = ChatPromptTemplate(
     [
         (
             "system",
-            "You are a helpful assistant that translates English to French. Translate the user sentence.",
+            "You are a helpful assistant that translates English to Japanese. Translate the user sentence.",
         ),
         MessagesPlaceholder(variable_name="chat_history"),
-        ("human", "{query} (think less)"),
+        (
+            "human",
+            "Translate the word into Japanese language: {query} (think less)",
+        ),
     ]
 )
 
-chat_history = []
 
-context = template | model
+async def run_model(model: ChatOpenAI, template: ChatPromptTemplate):
+    chat_history = []
 
-for i in range(100):
-    user_query = input("> ")
-    chat_history.append(user_query)
+    context = template | model
 
-    ai_msg = context.invoke({"query": user_query, "chat_history": chat_history})
-    chat_history.append(ai_msg.content)
-    print(ai_msg.content)
+    for _ in range(100):
+        user_query = input("> ")
+
+        gen_content = ""
+
+        async for chunk in context.astream(
+            {"query": user_query, "chat_history": chat_history}
+        ):
+            # gen_content += chunk.content
+            print(chunk.content, end="", flush=True)
+
+        chat_history.append(("human", user_query))
+        chat_history.append(("ai", gen_content))
+        print()
+
+
+asyncio.run(run_model(model, template))
