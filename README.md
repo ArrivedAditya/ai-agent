98230e26a964bd83636f522cb539dfbd802cd028
Aaditya Anand <aa3014429@gmail.com>
2026-03-01 11:40:23 +0530

    Added the chatbot support and updated the README.md file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ai-agent
+
+A basic and simple agent that uses rwkv models.
+
+## progress
+- [x] Implement a chatbot with system prompts.
+- [ ] Add RAG system with to it.
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 llm_name: str = "rwkv7-g1d"
 provider_url: str = "http://127.0.0.1:65530/api/oai"
 
 model = ChatOpenAI(model=llm_name, base_url=provider_url, api_key="Anything")
 
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that translates English to French. Translate the user sentence.",
-    ),
-    ("human", "I love programming."),
-]
-ai_msg = model.invoke(messages)
-print(ai_msg)
+template = ChatPromptTemplate(
+    [
+        (
+            "system",
+            "You are a helpful assistant that translates English to French. Translate the user sentence.",
+        ),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{query} (think less)"),
+    ]
+)
+
+chat_history = []
+
+context = template | model
+
+for i in range(100):
+    user_query = input("> ")
+    chat_history.append(user_query)
+
+    ai_msg = context.invoke({"query": user_query, "chat_history": chat_history})
+    chat_history.append(ai_msg.content)
+    print(ai_msg.content)
